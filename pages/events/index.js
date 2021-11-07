b0279ede4de6f88efe31b82d52d718e46157ee97
JeranDev <jeranjb@gmail.com>
2021-11-07 14:55:35 -0700

    Map and display events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,27 @@
 import Layout from '@/components/Layout'
+import EventItem from '@/components/EventItem'
+import { API_URL } from '@/config/index'
 
-export default function EventsPages() {
+export default function EventsPage({ events }) {
   return (
     <Layout>
-      <h1>My Events</h1>
+      <h1>Events</h1>
+      {events.lenth === 0 && <h3>No events to Show</h3>}
+
+      {events.map(evt => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/events`)
+  const events = await res.json()
+  console.log(events)
+
+  return {
+    props: { events },
+    revalidate: 1,
+  }
 }

34ae79d9eae849b5c25dba445a2d4f823cec2a33
JeranDev <jerandev@outlook.com>
2021-11-14 19:02:40 -0700

    Add event submit functionality

--- components/EventItem.js
+++ components/EventItem.js
@@ -10,7 +10,7 @@ export default function EventItem({ evt }) {
           src={
             evt.image
               ? evt.image.formats.thumbnail.url
-              : '/images/evt-default.png'
+              : '/images/event-default.png'
           }
           width={170}
           height={100}
